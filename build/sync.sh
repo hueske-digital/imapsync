96170b33bea6e7b7f5387c13d8add062bf938515
Tobias Hüske <t.hueske@mittwald.de>
2025-04-30 08:29:36 +0200

    Initial

---
+++ build/sync.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+: "${HOST1:?Missing HOST1}"
+: "${USER1:?Missing USER1}"
+: "${PASSWORD1:?Missing PASSWORD1}"
+: "${HOST2:?Missing HOST2}"
+: "${USER2:?Missing USER2}"
+: "${PASSWORD2:?Missing PASSWORD2}"
+
+echo "--------------------------------------------------"
+echo "Starte imapsync im Intervall von $SYNC_INTERVAL Sekunden..."
+echo "Quelle:      $USER1@$HOST1"
+echo "Ziel:        $USER2@$HOST2"
+echo "Parameter:   $EXTRA_PARAMS"
+echo "--------------------------------------------------"
+
+while true; do
+  echo "[$(date)] Starte Synchronisierung: $USER1@$HOST1 → $USER2@$HOST2"
+  imapsync \
+    --host1 "$HOST1" --user1 "$USER1" --password1 "$PASSWORD1" \
+    --host2 "$HOST2" --user2 "$USER2" --password2 "$PASSWORD2" \
+    $EXTRA_PARAMS
+  echo "[$(date)] Synchronisierung abgeschlossen."
+  sleep "$SYNC_INTERVAL"
+done
